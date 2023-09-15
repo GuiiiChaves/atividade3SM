71e889d72f1e51c232c18b2707144701a472a88f
Guilherme Chaves Sena <114080615+GuiiiChaves@users.noreply.github.com>
2023-09-15 19:44:58 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/outro.css',
+          '/assets/js/jquery.min.js',
+          // Adicione aqui outros recursos que deseja precachear
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
+
+  
